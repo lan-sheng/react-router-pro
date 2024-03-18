5e24a046390e6be4c980484acb0f05eb71f28d1d
lilansheng <945004642@qq.com>
2024-03-19 00:44:20 +0800

    路由导航

--- src/page/Login/index.js
+++ src/page/Login/index.js
@@ -1,4 +1,15 @@
+import { Link, useNavigate } from 'react-router-dom'
+
 const Login = () => {
-  return <div>登录</div>
+  const navigate = useNavigate()
+  return (
+    <div>
+      登录
+      {/* 声明式导航 */}
+      <Link to="/article">跳转文章页</Link>
+      {/* 编程式导航 */}
+      <button onClick={() => navigate('/article')}>跳转文章页</button>
+    </div>
+  )
 }
 export default Login
